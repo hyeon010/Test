7494477de67d5c185816634f479f7866748403ab
sanghyeon <ppppp5001@naver.com>
2024-08-12 19:42:30 +0900

    프로젝트 UI를 구현함

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -2,7 +2,7 @@ import './Editor.css'
 
 const Editor = () => {
     return(
-        <div className="">
+        <div className="Editor">
             <input placeholder="새로운 Todo..."/>
             <button>추가</button>
         </div>
--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,5 +1,19 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+
 const List = () => {
-    return<div>List</div>
+    return (
+        <div className="List">
+            <h4>Todo List 🌼</h4>
+            <input placeholder="검색어를 입력하세요" />
+            <div className="todos_wrapper" />
+            <div>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+            </div>
+        </div>
+    )
 }
 
 export default List
