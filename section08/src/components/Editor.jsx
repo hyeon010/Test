7860afc562e2d17e35fac1e06fafdfc2e831d175
sanghyeon <ppppp5001@naver.com>
2024-08-15 23:40:44 +0900

    기능 구현을 준비하고, Todo List를 렌더링하며, Todo를 추가,수정, 삭제할 수 있게 함.

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,39 @@
 import './Editor.css'
+import { useState, useRef } from 'react'
 
-const Editor = () => {
+const Editor = ({onCreate}) => {
+
+    const [content, setContent] = useState("")
+    const contentRef = useRef()
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value)
+    }
+
+
+    const onKeydown = (e) => {
+        if(e.keyCode === 13) {
+            onSubmit();
+        }
+    }  //엔터를 눌러도 컨텐츠를 추가할 수 있게
+
+    const onSubmit = () => {
+        if (content === ""){
+            contentRef.current.focus() //만약 컨텐츠 추가를 안하고 버튼을 누르면
+            return
+        }
+        onCreate(content)
+        setContent("") //컨텐츠를 추가하고 나면 컨텐츠 검색창 초기화 
+    }
+
     return(
         <div className="Editor">
-            <input placeholder="새로운 Todo..."/>
-            <button>추가</button>
+            <input ref = {contentRef} 
+            value = {content} 
+            onChange = {onChangeContent}
+            onKeyDown={onKeydown} 
+            placeholder="새로운 Todo..."/>
+            <button onClick={onSubmit}>추가</button>
         </div>
     )
 }
--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,17 +1,44 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({todos, onUpdate, onDelete}) => {
+
+    const [search, setSearch] = useState("")
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const getFilteredData = () => {
+        if (search === "") {
+            return todos
+        }
+        return todos.filter((todo) => todo.content.toLowerCase().includes(search.toLowerCase()))
+    } //검색을 했는데 todo에 있으면 T or F 반환, toLowerCase는 검색했을 때, 대소문자 구분 없이 검색하려고 사용
+
+    const filteredTodos = getFilteredData();
+
     return (
         <div className="List">
             <h4>Todo List 🌼</h4>
-            <input placeholder="검색어를 입력하세요" />
+            <input 
+                value = {search}
+                onChange={onChangeSearch}
+                placeholder="검색어를 입력하세요" 
+            />
             <div className="todos_wrapper" />
-            <div>
-                <TodoItem/>
-                <TodoItem/>
-                <TodoItem/>
-            </div>
+                {filteredTodos.map((todo)=>{
+                    return (
+                        <TodoItem
+                            key={todo.id} 
+                            {...todo} 
+                            onUpdate={onUpdate}
+                            onDelete={onDelete}
+                        />
+                    )
+                })}
+            <div/>
         </div>
     )
 }
